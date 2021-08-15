ccefce4eda55e82f3dc1e2b7cdab02fdc82ce805
fabioStori <fabstx@gmail.com>
2021-08-15 18:06:19 -0300

    config github account

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          is diva trans??
+          is diva trans?
         </a>
       </header>
     </div>
