520d999a14cc475104b4a4062996a1e5795f35a0
fabioStori <fabstx@gmail.com>
2021-08-15 18:40:09 -0300

    testing github actions

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/is diva trans??/i);
+  const linkElement = screen.getByText(/Testing Github Actions/i);
   expect(linkElement).toBeInTheDocument();
 });
