9edb52827a1359d0570de7a249a31ad3c35de13f
fabioStori <fabstx@gmail.com>
2021-11-09 19:59:16 -0300

    improvements

--- src/pages/itens/Itens.styles.js
+++ src/pages/itens/Itens.styles.js
@@ -5,9 +5,10 @@ export const useStyles = makeStyles((theme) => ({
     height: 'calc(100vh - 64.5px)',
     display: 'flex',
   },
-
   pageWrapper: {
     height: '100%',
     width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
   },
 }));
