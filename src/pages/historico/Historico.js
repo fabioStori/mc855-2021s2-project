464190594f88389a2ccd58304b39185eb0f77538
AdivairRamos <a193325@dac.unicamp.br>
2021-12-02 10:49:25 -0300

    switch para mostrar somente alertas nas movimentações

--- src/pages/historico/Historico.js
+++ src/pages/historico/Historico.js
@@ -1,5 +1,5 @@
 import { Devices, Sensors } from '@mui/icons-material';
-import { Box } from '@mui/material';
+import { Box, Switch, FormControlLabel  } from '@mui/material';
 import { GridActionsCellItem } from '@mui/x-data-grid';
 import axios from 'axios';
 import {
@@ -27,6 +27,7 @@ export default function Historico(props) {
   const { control } = methods;
   const styles = useStyles();
   const [isLoadingData, setIsLoadingData] = useState(false);
+  const [onlyAlerts, setOnlyAlerts] = useState(false);
   const [afterDateValue, setAfterDateValue] = useState(null);
   const [beforeDateValue, setBeforeDateValue] = useState(null);
   const [invalidDate, setInvalidDate] = useState(null);
@@ -101,6 +102,10 @@ export default function Historico(props) {
     });
   };
 
+  const toggleOnlyAlertSwitch = (event) => {          
+    setOnlyAlerts(event.target.checked);   
+  }
+
   const showDataGridLoading = () => {
     setIsLoadingData(true);
   };
@@ -115,21 +120,20 @@ export default function Historico(props) {
     const end_timestamp =
       new Date(searchParams.beforeDateValue).getTime() / 1000;
     const item_query = searchParams.searchItemValue;
-    const sensor_query = searchParams.searchSensorValue;
-
+    const sensor_query = searchParams.searchSensorValue;    
     showDataGridLoading();
     axios
-
       .post('https://api.invent-io.ic.unicamp.br/api/v1/search/event', {
         sensor_query: sensor_query.length ? sensor_query.join('|') : '.*',
         item_query: item_query.length ? item_query.join('|') : '.*',
         start_timestamp_range: start_timestamp ? start_timestamp : null,
         end_timestamp_range: end_timestamp ? end_timestamp : null,
         signal: abortController.signal,
+        alert_only: onlyAlerts
       })
       .then((response) => {
-        hideDataGridLoading();
-        const rows = response.data.map((row) => {
+        hideDataGridLoading(); 
+        const rows = response.data.map((row) => {           
           return {
             id: row._id,
             item: row.item,
@@ -182,6 +186,7 @@ export default function Historico(props) {
     searchSensorValue,
     searchItemValue,
     invalidDate,
+    onlyAlerts
   ]);
 
   return (
@@ -216,6 +221,17 @@ export default function Historico(props) {
               anterior à data em "Antes de"
             </p>
           )}
+          <div>            
+            <FormControlLabel control={
+              <Switch 
+                checked={onlyAlerts}
+                color='error' 
+                onChange={(event) => toggleOnlyAlertSwitch(event)}
+                />
+              } 
+              label="Somente alertas" />            
+          </div>
+          
         </div>
         <Box width={{ xs: '100%', md: '40%' }}>
           <MultipleTextInputs
